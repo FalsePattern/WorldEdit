b87c28fd19b9359b30ba391615a385eb04e0cb2b
FalsePattern <me@falsepattern.com>
2024-07-09 20:52:05 +0200

    nvm, going back to single project layout

--- build.gradle.kts
+++ build.gradle.kts
@@ -66,8 +66,33 @@ repositories {
     }
 }
 
+sourceSets {
+    main {
+        java {
+            srcDir("core/src/main/java")
+            srcDir("core/src/legacy/java")
+        }
+        resources {
+            srcDir("core/src/main/resources")
+        }
+    }
+    test {
+        java {
+            srcDir("core/src/test/java")
+        }
+    }
+}
+
 dependencies {
-    shadowImplementation(project(":core"))
+    // Core deps
+    implementation("org.yaml:snakeyaml:1.9")
+    implementation("org.mozilla:rhino:1.7.15")
+    implementation("de.schlichtherle:truezip:6.8.3")
+    implementation("com.sk89q:jchronic:0.2.4a")
+    implementation("com.sk89q.lib:jlibnoise:1.0.0")
+    implementation("com.thoughtworks.paranamer:paranamer:2.6")
+
+    // Forge deps
     implementationSplit("com.falsepattern:falsepatternlib-mc1.7.10:1.2.5")
 
     devOnlyNonPublishable("codechicken:forgemultipart-mc1.7.10:1.6.2-mega:dev")
@@ -75,10 +100,17 @@ dependencies {
     devOnlyNonPublishable("mega:carpentersblocks-mc1.7.10:3.4.1-mega:dev")
 
     runtimeOnlyNonPublishable("codechicken:notenoughitems-mc1.7.10:2.3.1-mega:dev")
+
+    //Testing
+    testImplementation("org.mockito:mockito-core:1.9.0")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.3")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
-afterEvaluate {
-    tasks.named<Jar>("sourcesJar").configure {
-        from(project(":core").sourceSets["main"].allSource)
+tasks.named<Test>("test") {
+    useJUnitPlatform()
+
+    testLogging {
+        events("passed")
     }
 }
